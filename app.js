601ba5cc05c0f87fdf97027a5cca609cde2acf6e
sujan budhathoki <sujanbudhathoki123@gmail.com>
2020-07-30 16:37:50 +0545

    prepared for deployment

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const helmet = require("helmet");
 const mongoSanitize = require("express-mongo-sanitize");
 const xss = require("xss-clean");
 const compression =  require('compression')
+const path = require('path')
 
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
@@ -37,9 +38,20 @@ app.use("/api/v1/users", userRouter);
 app.use("/api/v1/profile", profileRouter);
 app.use("/api/v1/posts", postRouter);
 
+if(process.env.NODE_ENV ===  'production'){
+ app.use(express.static('client/build'));
+
+ app.get('*', (req,res) => {
+   res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+ })
+
+}
+
 app.all("*", (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on the Server.`, 404));
 });
+
+
 
 app.use(globalErrorHandler);
 module.exports = app;
